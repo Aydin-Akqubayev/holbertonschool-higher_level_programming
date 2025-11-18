2208e3545070ab8b31c9211ec59040d6b0f3e829
Aydin-Akqubayev <aydinakqubayev1@gmail.com>
2025-11-18 12:34:06 +0400

    Infinite additionwq

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+
+    a = sys.argv[1:]
+    length = len(a)
+    i = 0
+    while i < len(a):
+        try:
+            a[i] = int(a[i])
+            i + =1
+        except Exception:
+            a.pop(i)
+        print(sum(a))
+
+
+if __name__ == "__main__":
+    main()
