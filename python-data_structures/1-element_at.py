18a611469245dea11ced7f2c55249bc1a7993421
Aydin-Akqubayev <aydinakqubayev1@gmail.com>
2025-11-18 19:16:41 +0400

    new

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    return my_list[idx]
+
+    try:
+        if idx >= 0:
+            return my_list[idx]
+
+        else:
+            return None
+
+    except IndexError:
+        return None 
