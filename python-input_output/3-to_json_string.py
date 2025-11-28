1905914a59fbb37ea4f365c3531f02ffeadf9739
Aydin-Akqubayev <aydinakqubayev1@gmail.com>
2025-11-28 20:27:16 +0400

    new

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+
+'''To JSON string'''
+
+
+def to_json_string(my_list):
+
+    '''Json practice with python '''
+
+    new_json = json.dumps(my_list)
+    return new_json
