9912a0facd9d8ba7c36dbe79bb51a5caf9aeccd8
Aydin-Akqubayev <aydinakqubayev1@gmail.com>
2025-11-30 02:08:36 +0400

    new

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+
+    '''Your code here to serialize and save data to the specified file'''
+
+    json_file = json.dumps(data)
+    with open(filename, 'w') as f:
+        f.write(json_file)
+
+
+def load_and_deserialize(filename):
+
+    ''' Your code here to load and deserialize data from the specified file'''
+
+    with open(filename, 'r') as f:
+        json_file = f.read()
+    return json.loads(json_file)
