f25d670473d8cefefb24c7e8396a8b6a05f1bafa
Aydin-Akqubayev <aydinakqubayev1@gmail.com>
2025-11-30 00:46:45 +0400

    new

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+def pascal_triangle(num):
+
+    '''Generates Pascal's triangle for a given number of rows.'''
+
+    res = []
+
+    if num == 0:
+        return res
+
+    res.append([1])
+
+    for i in range(1, num):
+        row = [1]
+        for j in range(1, len(res[i-1])):
+            row.append(res[i-1][j-1] + res[i-1][j])
+        row.append(1)
+        res.append(row)
+
+    return res
