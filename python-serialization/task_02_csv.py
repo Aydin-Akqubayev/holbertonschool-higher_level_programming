2824a14ee013e2c3f4697afead1f8aaa57957014
Aydin-Akqubayev <aydinakqubayev1@gmail.com>
2025-11-30 12:00:23 +0400

    new

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -7,12 +7,10 @@ import csv
 
 def convert_csv_to_json(csv_file):
     try:
-
-        with open(csv_file, 'r') as f:
-            r = csv.DictReader(f)
-            res =[]
-            for i in r:
-                res.append(i)
-            return res
-    except FileNotFoundError:
+        with open(csv_file, "r", newline="") as f:
+            r = list(csv.DictReader(f))
+        with open("data.json", "w") as f:
+            json.dump(r, f, indent=4)
+        return True
+    except Exception:
         return False
