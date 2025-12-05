cc2da01ffb6de12eb00f6eef284ffdaa74d48cca
Aydin-Akqubayev <aydinakqubayev1@gmail.com>
2025-12-05 20:38:28 +0400

    update version

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ with urlopen(req) as response:
     html = response.read()
 
 print("Body response:")
-print(f"\t - type: {type(html)}")
-print(f"\t - content: {html}")
-print(f"\t - utf8 content: {html.decode()}")
+print(f"\t- type: {type(html)}")
+print(f"\t- content: {html}")
+print(f"\t- utf8 content: {html.decode()}")
