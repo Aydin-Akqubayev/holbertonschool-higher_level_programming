b5a7493a8cf9cb1da0e8892f1e5feda12f93aa18
Aydin-Akqubayev <aydinakqubayev1@gmail.com>
2025-12-05 20:31:20 +0400

    urllib-practice

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from urllib.request import Request, urlopen
+
+website_url = 'https://intranet.hbtn.io/status'
+req = Request(website_url)
+with urlopen(req) as response:
+    html = response.read()
+
+print("Body response:")
+print(f"\t - type: {type(html)}")
+print(f"\t - content: {html}")
+print(f"\t - utf8 content: {html.decode()}")
