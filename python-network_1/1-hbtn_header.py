f687be0b4148b02612b4e44a56033c60e1a7b1e9
Aydin-Akqubayev <aydinakqubayev1@gmail.com>
2025-12-06 20:12:55 +0400

    update-version code X-Request-id

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -2,8 +2,14 @@
 from urllib.request import Request, urlopen
 import sys
 
-url = sys.argv[1]
-req = Request(url)
-with urlopen(req) as response:
-    html = response.getheader("X-Request-Id")
-print(html)
+
+def main():
+    url = sys.argv[1]
+    req = Request(url)
+    with urlopen(req) as response:
+        html = response.getheader("X-Request-Id")
+    print(html)
+
+
+if __name__ == '__main__':
+    main()
