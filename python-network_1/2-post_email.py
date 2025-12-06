291e43db66d8d82ddac149eb822adb4156e5f3f5
Aydin-Akqubayev <aydinakqubayev1@gmail.com>
2025-12-06 20:46:45 +0400

    update version post request

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -5,17 +5,22 @@ from urllib.request import Request, urlopen
 import urllib.parse
 import sys
 
-url = sys.argv[1]
-mail_address = sys.argv[2]
-print(url)
-data = {
-    "email": mail_address
-}
 
-data = urllib.parse.urlencode(data).encode('utf-8')
+def main():
+    url = sys.argv[1]
+    mail_address = sys.argv[2]
 
-request = Request(url, method="POST", data=data)
-with urlopen(request) as response:
-    html = response.read().decode('utf-8')
+    data = {
+        "email": mail_address
+    }
 
-    print(html)
+    data = urllib.parse.urlencode(data).encode('utf-8')
+
+    request = Request(url, method="POST", data=data)
+    with urlopen(request) as response:
+        html = response.read().decode('utf-8')
+        print(html)
+
+
+if __name__ == '__main__':
+    main()
